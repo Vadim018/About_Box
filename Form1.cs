41910c5ae9a56b6cef3b5dbe06e6ea1991cb02b7
Vadim Voitsehovskiy <vadim.rolex.2005@gmail.com>
2023-12-27 16:06:23 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пункт меню Об'єкт");
+        }
+
+        private void довідкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox = new AboutBox1();
+            aboutBox.ShowDialog(this);
+
+        }
+    }
+}
